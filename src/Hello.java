223e1399dba3db6d26ec0f9ef03c983d1c04ffdb
lindu <lindu@lm-sfa-11021114.corp.ebay.com>
2021-10-08 16:02:15 -0700

    update

--- src/Hello.java
+++ src/Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
-        for(int i=0;i<10;i++)
+        for(int i=0;i<20;i++)
             System.out.println("The number: "+i);
     }
 }
